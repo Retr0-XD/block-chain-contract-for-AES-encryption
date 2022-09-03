51b6fc0bd3e49405df8bd006bc14dfcbefcc1297
Retr0-XD <88275564+Retr0-XD@users.noreply.github.com>
2022-09-03 19:52:56 +0530

    main
    this consist of encryption decryption and integrity contracts which will work  under block chain medium and which will perform 
    aes encryption with lightweight fucntions..

---
+++ divinity.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.7.0;
+contract HelloWorld {
+    function sayMessage() public pure returns (string memory) {
+        return "My block chains goes Durrr.....";
+    }
+}
---
+++ dsvkns.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.7.0;
+
+contract Counter {
+    uint public count;
+
+    function get() public view returns (uint) {
+        return count;
+    }
+
+
+    function inc() public {
+        count += 1;
+    }
+
+    
+    function dec() public {
+        
+        count -= 1;
+    }
+}
